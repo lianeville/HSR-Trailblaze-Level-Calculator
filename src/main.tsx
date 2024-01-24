072177cf4368abf84847e7efa7124cfe61409184
Lia Neville <lianeville5@gmail.com>
2024-01-24 16:26:53 -0500

    adds analytics (hopefully)

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+		<Analytics />
+	</React.StrictMode>
 )
