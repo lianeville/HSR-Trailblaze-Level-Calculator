b9cd6df55531491ee1257ae17b9ce876e413402a
Lia Neville <lianeville5@gmail.com>
2024-01-24 16:30:15 -0500

    doesnt add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,11 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store'
-import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<App />
 		</Provider>
-		<Analytics />
 	</React.StrictMode>
 )
